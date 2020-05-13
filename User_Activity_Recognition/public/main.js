c9e529d9e0a0ff41bd7a9c2260b7c4853903992e
nicoDs96 <disanto.1853048@studenti.uniroma1.it>
2020-05-13 14:53:13 +0200

    Added Paho sketch

--- User_Activity_Recognition/public/main.js
+++ User_Activity_Recognition/public/main.js
@@ -1,28 +1,59 @@
+const crypto = require('crypto');
+
 let alert = '<div class="alert alert-warning" role="alert">'+
   'Seems like if your device/browser do not support accelerometer. Try to change device/browser.'+
   '</div>';
 
-$(document).ready(function() {
+var activityTopic = ""; //pattern sensor/{client id}/position
+var sensorPubTopic = ""; //pattern sensor/{client id}/accelerometer
+var clientUniqueId = "";
+$(document).ready(async function() {
   try {
     // Create Sensor
     let sensor = new Accelerometer({frequency:1});
     sensor.onerror = event => console.log(event.error.name, event.error.message);
+    
+    // allow user to start stop monitoring
     $('#stop').click( () =>{sensor.stop()} );
     $('#start').click( () =>{sensor.start()} );
     
-    sensor.onreading = () => {
-        console.log("Acceleration along X-axis: " + sensor.x);
-        $('#x').text(sensor.x);
-        console.log("Acceleration along Y-axis: " + sensor.y);
-        $('#y').text(sensor.y);
-        console.log("Acceleration along Z-axis: " + sensor.z);
-        $('#z').text(sensor.z);
-    }
+    // Paho configuration
+    let location = {hostname:"127.0.0.1", port:"16396", awsClientID: "client"};
+    client = new Paho.MQTT.Client(location.hostname, Number(location.port),"/wss", location.awsClientID);
     
-    //sensor.start()
-    
-    //eventually add: sensor.stop()
+    // Init parameters
+    var clientUniqueId = await getUniqueId();
+    var activityTopic = `sensor/${clientUniqueId}/position`; 
+    var sensorPubTopic = `sensor/${clientUniqueId}/accelerometer`;
 
+    // set callback handlers
+    client.onConnectionLost = onConnectionLost;
+    client.onMessageArrived = onMessageArrived;
+    client.connect({onSuccess:onConnect});
+
+
+    // TODO: 3. update view with classification info (onmessage callback)
+    
+    sensor.onreading = () => {
+        //console.log("Acceleration along X-axis: " + sensor.x);
+        $('#x').text(sensor.x);
+        //console.log("Acceleration along Y-axis: " + sensor.y);
+        $('#y').text(sensor.y);
+        //console.log("Acceleration along Z-axis: " + sensor.z);
+        $('#z').text(sensor.z);
+        
+        //Create a message
+        let msgText = JSON.stringify({clientId:clientUniqueId,x:sensor.x,y:sensor.y,z:sensor.z});
+        message = new Paho.MQTT.Message( msgText );
+        message.destinationName = sensorPubTopic;
+        //Send it with paho
+        client.send(message);
+        
+        //TODO: 4. display data graphically
+
+      
+    }
+  
 
     } catch(error) {
         console.log('Error creating sensor:')
@@ -32,8 +63,34 @@ $(document).ready(function() {
         //Fallback, do something else etc.
     }
 
-   
   
 });
 
+// called when the client connects
+function onConnect() {
+  // Once a connection has been made, make a subscription and send a message.
+  client.subscribe(activityTopic);
+  
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+      console.log("onConnectionLost:"+responseObject.errorMessage);
+  }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+  console.log("onMessageArrived:"+message.payloadString);
+}
+
+async function getUniqueId(){
+
+  const msgUint8 = new TextEncoder().encode(new Date().toLocaleString()+Math.random().toString());                           // encode as (utf-8) Uint8Array
+  const hashBuffer = await crypto.subtle.digest('SHA-1', msgUint8);           // hash the message
+  const hashArray = Array.from(new Uint8Array(hashBuffer));                     // convert buffer to byte array
+  return hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+}
 
